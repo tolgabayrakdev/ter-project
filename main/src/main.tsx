3b39ffb6f7b85b2cd548ba55faf046a0e1a88e27
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-01-21 22:31:24 +0300

    pages

--- main/src/main.tsx
+++ main/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { lazy, Suspense } from 'react'
+
+const Home = lazy(() => import('./pages/home'));
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </Suspense>
+
 )
